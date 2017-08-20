6aff5ed299fb29570026d2231a61f9a0299936fa
mohit <mohitleo9@gmail.com>
2017-08-20 18:33:00 -0400

    eslint

--- jsclass.js
+++ jsclass.js
@@ -1,104 +1,102 @@
 (function(mod) {
 	if (typeof exports == 'object' && typeof module == 'object')  // CommonJS
-		return mod(require.main.require('tern/lib/infer'), require.main.require('tern/lib/tern'));
+	    return mod(require.main.require('tern/lib/infer'), require.main.require('tern/lib/tern'));
 
 	if (typeof define == 'function' && define.amd) // AMD
-		return define([ 'tern/lib/infer', 'tern/lib/tern' ], mod);
+	    return define([ 'tern/lib/infer', 'tern/lib/tern' ], mod);
 	mod(tern, tern);
 })(function(infer, tern) {
 	'use strict';
 
 	tern.registerPlugin('jsclass', function(server, options) {
-		server.addDefs(defs);
+	    server.addDefs(defs);
 	});
 
 	function getClassName(args, argNodes, cx) {
-		if (args[0] === cx.str)
-			return argNodes[0].raw;
+	    if (args[0] === cx.str)
+		  return argNodes[0].raw;
 
 	};
 
 	function propagateProperties(source, destination) {
 
-		const cx = infer.cx();
+	    const methods =  source.getProp('methods');
+	    const fields = source.getProp('fields');
+	    methods.forAllProps(function(prop, val, local) {
+		  // let's focus on just the local ones for now
+		  if (!local) return;
 
-		const methods =  source.getProp('methods');
-		const fields = source.getProp('fields');
-		methods.forAllProps(function(prop, val, local) {
-      // let's focus on just the local ones for now
-			if (!local) return;
-
-      // connect the value to destination prop
-      // destination.prop = val
-			val.propagate(destination.defProp(prop));
-		});
+		  // connect the value to destination prop
+		  // destination.prop = val
+		  val.propagate(destination.defProp(prop));
+	    });
 
 
-		fields.forAllProps(function(prop, val, local) {
-      // let's focus on just the local ones for now
-			const valType = val.getType();
-			if (!local) return;
+	    fields.forAllProps(function(prop, val, local) {
+		  // let's focus on just the local ones for now
+		  const valType = val.getType();
+		  if (!local) return;
 
-      // the 'type' of this field that we are interested in.
-			let typeAlreadySet;
+		  // the 'type' of this field that we are interested in.
+		  let typeAlreadySet;
 
-			if (valType && valType.originNode && valType.hasProp('type')) {
-				destination.defProp(prop).addType(valType.getProp('type'));
-				typeAlreadySet = true;
+		  if (valType && valType.originNode && valType.hasProp('type')) {
+			destination.defProp(prop).addType(valType.getProp('type'));
+			typeAlreadySet = true;
 
-	// valType.originNode.properties.forEach(({ key, value }) => {
-	//   if (key.name && key.name === 'type') {
-	//     if (value.raw) {
-	//       destination.defProp(prop).addType(cx.str);
-	//       typeAlreadySet = true;
-	//     }
-	//   }
-	// });
+			// valType.originNode.properties.forEach(({ key, value }) => {
+			//   if (key.name && key.name === 'type') {
+			//     if (value.raw) {
+			//       destination.defProp(prop).addType(cx.str);
+			//       typeAlreadySet = true;
+			//     }
+			//   }
+			// });
 
-			}
+		  }
 
-      // connect the value to destination prop
-      // destination.prop = val
-			!typeAlreadySet && val.propagate(destination.defProp(prop));
-		});
+		  // connect the value to destination prop
+		  // destination.prop = val
+		  !typeAlreadySet && val.propagate(destination.defProp(prop));
+	    });
 	};
 
 	infer.registerFunction('createCustomClass', function(_self, args, argNodes) {
-		let customClass, newProperties, classPrototype;
-		const cx = infer.cx();
+	    let customClass;
+	    const cx = infer.cx();
 
-		if (args[0].getType() instanceof infer.Fn)
-      // we are just extending props.
-			customClass = args[0].getType();
+	    if (args[0].getType() instanceof infer.Fn)
+		  // we are just extending props.
+		  customClass = args[0].getType();
 
-		else
-      customClass = new infer.Fn(getClassName(args, argNodes, cx), new infer.Obj(true), [], [], new infer.AVal());
+	    else
+		  customClass = new infer.Fn(getClassName(args, argNodes, cx), new infer.Obj(true), [], [], new infer.AVal());
 
 
-		if (args.length <= 1)
-			return customClass;
+	    if (args.length <= 1)
+		  return customClass;
 
-		newProperties = args[1];
-		classPrototype = customClass.getProp('prototype').getType();
+	    const newProperties = args[1];
+	    const classPrototype = customClass.getProp('prototype').getType();
 
-		propagateProperties(newProperties, classPrototype);
-		return customClass;
+	    propagateProperties(newProperties, classPrototype);
+	    return customClass;
 	});
 
 
 	const defs = {
-		'!name'   : 'jsclass',
-		'!define' : {
-			'!known_modules' : {
-				JSClass : {
-	  '!type' : 'JS',
-				},
+	    '!name'   : 'jsclass',
+	    '!define' : {
+		  '!known_modules' : {
+			JSClass : {
+			      '!type' : 'JS',
 			},
-		},
-		'JS' : {
-			class : {
-				'!type' : 'fn(properties: ?) -> !custom:createCustomClass',
-			},
-		},
+		  },
+	    },
+	    'JS' : {
+		  class : {
+			'!type' : 'fn(properties: ?) -> !custom:createCustomClass',
+		  },
+	    },
 	};
 });
